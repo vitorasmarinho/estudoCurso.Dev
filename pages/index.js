fcdd6b350fb0e22897be66f72b1b4be3f8e4beb8
Vitor Marinho <vitor@vasm.me>
2025-05-08 18:27:29 +0000

    adiciona a pagina pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Que bom que tenho voce Rayane! Te amo imenso. Desejo muito ter voce sempre :)</h1>
+    return <h1>Que bom que tenho voce Rayane! Te amo imenso. Desejo muito ter voce sempre :) :) :)</h1>
 }
 
 export default Home;
